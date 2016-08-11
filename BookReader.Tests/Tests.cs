416393a6897df48918a02a2498f51249dc0c1a30
paullewisn <paul-lewis.nunn@comparethemarket.com>
2016-08-11 17:01:53 +0100

    first test failed

---
+++ BookReader.Tests/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BookReader.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Reader _reader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _reader = new Reader();
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void CountAllWords(string input, int knownCount)
+        {
+            Assert.That(_reader.CountWords(input, string.Empty), Is.EqualTo(knownCount + 1));
+        }
+
+        public object[] TestCases =
+        {
+            new object[]
+            {
+                "In 2003, for the first time since the Second World War, the United Kingdom took part in an opposed invasion and full‑scale occupation of a sovereign State – Iraq.Cabinet decided on 17 March to join the US‑led invasion of Iraq, assuming there was no last‑minute capitulation by Saddam Hussein.That decision was ratified by Parliament the next day and implemented the night after that. ",
+                64
+            }
+        };
+    }
+}
---
+++ BookReader/Reader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookReader
+{
+    public class Reader
+    {
+        public int CountWords(string input, string query)
+        {
+            var words = input.Split(' ');
+            return words.Length;
+        }
+    }
+}
